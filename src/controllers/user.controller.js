a9e91e3f62c7875d8dd86cecc2dedbc0aa001106
hrnns-ti <nassjourney@gmail.com>
2026-08-07 20:29:04 +0700

    feat: forgot pass - users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,8 +7,6 @@ async function signin(req, res) {
   const password = req.body.password;
   
   try {
-
-
     const sql = 'select * from users where email=?';
     const values = [email];
     const [result] = await db.promise().execute(sql, values);
@@ -50,24 +48,19 @@ async function signup(req, res) {
   const passwordMatch = req.body.passwordMatch;
 
   try {
-    if (password !== passwordMatch) {
-      return res.status(400).json({
-        status: false,
-        message: "Password doesn't match"
-      });
-    }
-  
     const salt = 10;
     const hashedPassword = await bcrypt.hash(password, salt);
-
-    // const emailSQL = 'select * from users where email=?';
-    // const emailValues = [email];
-    // const [result] = await db.promise().execute(sql, values);
-
   
     const sql = 'insert into users(fullname, email, password) values (?, ?, ?)';
     const values = [fullname, email, hashedPassword];
     const [result] = await db.promise().execute(sql, values);
+
+    if (result.length === 0) {
+      return res.status(401).json({
+          status: false,
+          message: "Email didn't match"
+      });
+    }
   
     res.status(201).json({
       status: true,
@@ -90,10 +83,36 @@ async function signup(req, res) {
 
 // forgot password
 async function forgotPass(req, res) {
-  
+  const fullname = req.body.fullname;
+  const email = req.body.email;
+  const password = req.body.password;
 
-  const salt = 7
-  const hashedPassword = bcrypt.hash(password)
+  try {
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(password, salt);
+    
+    const sql = 'update users set password = ? where email = ? and fullname = ?';
+    const values = [hashedPassword, email, fullname];
+    const [result] = await db.promise().execute(sql, values);
+
+    if (result.affectedRows === 0) {
+      return res.status(401).json({
+          status: false,
+          message: "Email or fullname didn't match"
+      });
+    }
+  
+    res.status(200).json({
+      status: true,
+      data: result
+    });
+    
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: 'Change password failed: ' + error.message
+    }); 
+  }
 }
 
 module.exports = {
