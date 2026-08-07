eb90377653881494251834e9194b3ec8175521b1
hrnns-ti <nassjourney@gmail.com>
2026-08-07 08:44:48 +0700

    feat: delete message - contact

--- src/controllers/contact-controller.js
+++ src/controllers/contact-controller.js
@@ -10,26 +10,50 @@ async function createMessage(req, res) {
   try {
     const sql = 'insert into messages(name,email, phone, subject, message) VALUES (?, ?, ?, ?, ?)';
     const values = [name, email, phone, subject, message];
-
     const [result, fields] = await db.promise().execute(sql, values);
+    
     console.log(result);
-    console.log(fields);
-
+    
     res.status(201).json({
       success: true,
       message: 'Message has sent successfully',
       dataID: result.insertId
     });
-
+    
   } catch (error) {
     console.error('Error while inserting message:', error);
     res.status(500).json({
       success: false,
-      error: 'Message sent failed: ', error,
+      error: 'Message sent failed: ' + error,
+    });
+  }
+}
+
+async function deleteMessage(req, res) {
+  const messageID = req.body.id;
+  
+  try {
+    const sql = 'delete from messages where id=?';
+    const values = [messageID];
+    const [result] = await db.promise().execute(sql, values);
+  
+    console.log(result)
+
+    res.status(201).json({
+      success: true,
+      message: 'Message deleted',
+      dataID: result.insertId
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: 'Message has fail to deleted: ' + error,
     });
   }
 }
 
 module.exports = {
-  createMessage
+  createMessage,
+  deleteMessage,
 }
--- src/routes/contact-route.js
+++ src/routes/contact-route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { createMessage } = require('../controllers/contact-controller');
+const { createMessage, deleteMessage } = require('../controllers/contact-controller');
 
 router.post('/send', createMessage);
+router.post('/delete', deleteMessage);
 
 module.exports = router
