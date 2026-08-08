b5d59449010470c81b04300bc852169dd3430280
hrnns-ti <nassjourney@gmail.com>
2026-08-08 23:11:21 +0700

    feat: change status - donations

--- src/controllers/donation.controller.js
+++ src/controllers/donation.controller.js
@@ -26,7 +26,7 @@ async function createDonation(req, res) {
 
 async function getDonations(req, res) {
   try {
-   const sql = 'select provider_id, food_name, quantity, pickup_location, status, created_at from donations'
+   const sql = `select provider_id, food_name, quantity, pickup_location, status, created_at from donations order by field(status, 'pending', 'completed'), created_at asc`
    const [result] = await db.promise().execute(sql)
    
    res.status(201).json({
@@ -42,7 +42,32 @@ async function getDonations(req, res) {
   }
 }
 
+async function changeStatus(req, res) {
+  const status = req.body.status
+  const id = req.body.id
+  try {
+   const sql = 'update donations set status = ? where id = ?'
+   const values = [status, id]
+   const [result] = await db.promise().execute(sql, values)
+   
+   res.status(201).json({
+    status: true,
+    message: 'Donation created successfully!',
+    data: result
+   })
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: 'Failed to change booking status: ' + error
+    })
+  }
+}
+
+
+
 module.exports = {
   createDonation,
   getDonations,
+  changeStatus,
+  myDonations
 }
--- src/routes/donation.route.js
+++ src/routes/donation.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { createDonation, getDonations } = require('../controllers/donation.controller')
+const { createDonation, getDonations, changeStatus } = require('../controllers/donation.controller')
 const { verifyToken, isAdmin } = require('../middlewares/auth.middleware');
 
 router.post('/apply', verifyToken, createDonation);
 router.get('/list', verifyToken, isAdmin, getDonations);
+router.post('/status', verifyToken, isAdmin, changeStatus);
+router.get('/mine', verifyToken, getDonations);
 
 module.exports = router;
